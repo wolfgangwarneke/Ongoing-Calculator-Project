b6bd06565608de08f43e68091ed484c632c2d9f3
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-02 01:12:55 -0700

    add basic file tree and add basic arithmetic functions to script.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//MATH FUNCTIONS
+//addition
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+//subtraction
+var sub = function(number1, number2) {
+	return number1 - number2;
+};
+
+// multiplication
+function times(number1, number2) {
+	return number1 * number2;
+}
+
+//division
+function div(number1, number2) {
+	return number1 / number2;
+}
+//END MATH FUNCTIONS
